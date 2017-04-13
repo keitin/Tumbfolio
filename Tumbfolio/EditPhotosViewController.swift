8e26b890aefc0ea2d1793e6379ec1555baac131a
松下慶大 <atiek1121@gmail.com>
2017-04-13 15:01:17 +0900

    Make keyborad closed when we touch the screen and scroll it

--- Tumbfolio/EditPhotosViewController.swift
+++ Tumbfolio/EditPhotosViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EditPhotosViewController: UIViewController {
+class EditPhotosViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     var editors: [PhotoInfoEditor] = []
@@ -25,10 +25,13 @@ class EditPhotosViewController: UIViewController {
         
         title = "Edit Photos"
         
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(EditPhotosViewController.tapCreateButton(sender:)))
         
         fillValuesIntoEditor()
+        
+        // MARK: Add Tap Gesture
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(EditPhotosViewController.tapScreen(sender:)))
+        self.view.addGestureRecognizer(gesture)
     }
     
     override func viewDidLayoutSubviews() {
@@ -47,6 +50,14 @@ class EditPhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tapScreen(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        self.view.endEditing(true)
+    }
+    
     private func fillValuesIntoEditor() {
         for index in 0...selectedPosts.numberOfPosts - 1 {
             let editor = PhotoInfoEditor.makeInstance()
@@ -58,6 +69,7 @@ class EditPhotosViewController: UIViewController {
     
     private func designScrollView() {
         self.scrollView.contentSize = CGSize(width: scrollView.frame.width * CGFloat(selectedPosts.numberOfPosts), height: scrollView.frame.height - navigationController!.navigationBar.frame.height)
+        self.scrollView.delegate = self
         for index in 0...selectedPosts.numberOfPosts - 1 {
             let editor = editors[index]
             editor.draw(rect: scrollView.frame, position: index)
